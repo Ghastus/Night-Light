74d84798de3eaba3c528d55643affe81def0bcfb
Ghastus <benjp.1122@gmail.com>
2018-04-22 15:00:44 -0400

    Boss Room & Nav Mesh
    Added foundation for new boss room as well as nav mesh content

---
+++ Assets/Scripts/BossMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class BossMovement : MonoBehaviour {
+    [SerializeField]
+    Transform _destination;
+
+    NavMeshAgent _navMeshAgent;
+    Vector3 targetVector;
+    // Use this for initialization
+    void Start () {
+        _navMeshAgent = this.GetComponent<NavMeshAgent>();
+		if(_navMeshAgent == null)
+        {
+            Debug.LogError("nav mesh agent is not attached to " + gameObject.name);
+        }
+        else
+        {
+            SetDestination();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        SetDestination();
+        
+	}
+
+    private void SetDestination()
+    {
+        targetVector = _destination.transform.position;
+        _navMeshAgent.SetDestination(targetVector);
+    } 
+}
